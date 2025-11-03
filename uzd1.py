97018d44d67ce5b788e4d429c6dc74061fe5f0d9
Dtoce <cape@inbox.lv>
2025-11-03 11:52:26 +0200

    Apskatīju 1.uzdevumu

--- uzd1.py
+++ uzd1.py
@@ -1,7 +1,7 @@
+# Sasummē ievadītos skaitļus. 0 norāda, ka darbs jābeidz.
 summa = 0
 x = int(input("Ievadi skaitli (0 lai beigtu): "))
 while x != 0:
     summa += x
     x = int(input("Ievadi skaitli (0 lai beigtu): "))
 print("Summa:", summa)
-``
